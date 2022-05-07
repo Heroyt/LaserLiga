310eabbf53187cac3ddf575e26e13f317b1f7460
Tomáš Vojík <vojik@wboy.cz>
2022-05-07 16:16:27 +0200

    :egg: NGGYU

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,10 @@ Route::get('/', [Dashboard::class, 'show'])->name('dashboard');
 Route::get('/game', [Games::class, 'show'])->name('game-empty');    // This will result in HTTP 404 error
 Route::get('/g', [Games::class, 'show'])->name('game-empty-alias'); // This will result in HTTP 404 error
 Route::get('/game/{game}', [Games::class, 'show'])->name('game');
+Route::get('/g/abcdefghij', static function() {
+	header('location: https://youtu.be/dQw4w9WgXcQ');
+	exit;
+});
 Route::get('/g/{game}', [Games::class, 'show'])->name('game-alias');
 Route::get('players/leaderboard', [Games::class, 'todayLeaderboard']);
 Route::get('players/leaderboard/{system}', [Games::class, 'todayLeaderboard']);
