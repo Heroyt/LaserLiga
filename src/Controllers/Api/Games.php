db96ac62e0befe096e2f91ad8da20278de239b9c
Tomáš Vojík <vojik@wboy.cz>
2022-05-28 15:11:53 +0200

    :sparkles: List game API filters

--- src/Controllers/Api/Games.php
+++ src/Controllers/Api/Games.php
@@ -3,6 +3,7 @@
 namespace App\Controllers\Api;
 
 use App\Core\ApiController;
+use App\Core\App;
 use App\Core\Interfaces\RequestInterface;
 use App\Core\Middleware\ApiToken;
 use App\Core\Request;
@@ -40,18 +41,195 @@ class Games extends ApiController
 	 * @return void
 	 */
 	public function listGames(Request $request) : void {
-		$date = null;
-		if (!empty($request->get['date'])) {
-			try {
-				$date = new DateTime($request->get['date']);
-			} catch (Exception $e) {
-				$this->respond(['error' => 'Invalid parameter: "date"', 'exception' => $e->getMessage()], 400);
+		$notFilters = ['date', 'system', 'sql', 'returnLink', 'returnCodes'];
+		try {
+			$date = null;
+			if (!empty($request->get['date'])) {
+				try {
+					$date = new DateTime($request->get['date']);
+				} catch (Exception $e) {
+					$this->respond(['error' => 'Invalid parameter: "date"', 'exception' => $e->getMessage()], 400);
+				}
 			}
-		}
-		// TODO: Possibly more filters
-		$query = GameFactory::queryGames(false, $date)->where('%n = %i', Arena::PRIMARY_KEY, $this->arena->id);
 
-		$games = $query->fetchAll();
+			if (!empty($request->get['system'])) {
+				$query = GameFactory::queryGamesSystem($request->get['system'], true);
+				if (isset($date)) {
+					$query->where('DATE([start]) = %d', $date);
+				}
+			}
+			else {
+				$query = GameFactory::queryGames(false, $date);
+			}
+			$query->where('%n = %i', Arena::PRIMARY_KEY, $this->arena->id);
+
+			// TODO: Filter parsing could be more universally implemented for all API Controllers
+			$availableFilters = GameFactory::getAvailableFilters($request->get['system'] ?? null);
+			foreach ($request->get as $field => $value) {
+				$not = str_starts_with($value, 'not');
+				if ($not) {
+					$value = substr($value, 3);
+				}
+				if (in_array($field, $notFilters, true) || !in_array($field, $availableFilters, true)) {
+					continue;
+				}
+				if (is_array($value)) {
+					$query->where('%n '.($not ? 'NOT ' : '').'IN %in', Strings::toSnakeCase($field), $value);
+					continue;
+				}
+
+				$cmp = $value[0];
+				switch ($cmp) {
+					case '>':
+					case '<':
+						if ($value[1] === '=') {
+							$cmp .= '=';
+							$value = substr($value, 2);
+							break;
+						}
+						$value = substr($value, 1);
+						break;
+					default:
+						$cmp = $not ? '<>' : '=';
+				}
+
+				// Check for BETWEEN operator
+				if (str_contains($value, '~')) {
+					if ($cmp !== '<>' && $cmp !== '=') {
+						$this->respond(
+							[
+								'error'       => 'Invalid filter',
+								'description' => 'Field "'.$field.'" is formatted to use a `BETWEEN` operator and a `'.$cmp.'` operator.',
+								'value'       => $request->get['field'],
+							],
+							400);
+					}
+					$values = explode('~', $value);
+
+					// Check values
+					$type = '';
+					if (count($values) !== 2) {
+						$this->respond(
+							[
+								'error'       => 'Invalid filter',
+								'description' => 'Field "'.$field.'" must have exactly two values to use the `BETWEEN` operator.',
+								'value'       => $request->get['field'],
+							],
+							400);
+					}
+					foreach ($values as $v) {
+						if (empty($type)) {
+							if (is_numeric($v)) {
+								$type = 'int';
+								continue;
+							}
+							if (strtotime($v) > 0) {
+								$type = 'date';
+								continue;
+							}
+							$this->respond(
+								[
+									'error'       => 'Invalid filter',
+									'description' => 'Field "'.$field.'" must be a number or a date to use the BETWEEN operator.',
+									'value'       => $request->get['field'],
+								],
+								400);
+						}
+
+						if (is_numeric($v)) {
+							if ($type === 'int') {
+								continue;
+							}
+							$this->respond(
+								[
+									'error'       => 'Invalid filter',
+									'description' => 'First value is a date, but the second is a number in field "'.$field.'" for the BETWEEN operator.',
+									'value'       => $request->get['field'],
+								],
+								400);
+						}
+						if (strtotime($v) > 0) {
+							if ($type === 'date') {
+								continue;
+							}
+							$this->respond(
+								[
+									'error'       => 'Invalid filter',
+									'description' => 'First value is a number, but the second is a date in field "'.$field.'" for the BETWEEN operator.',
+									'value'       => $request->get['field'],
+								],
+								400);
+						}
+						$this->respond(
+							[
+								'error'       => 'Invalid filter',
+								'description' => 'Invalid type for BETWEEN operator for field "'.$field.'". The only accepted values are dates and numbers.',
+								'value'       => $request->get['field'],
+							],
+							400);
+					}
+
+					if ($type === 'int') {
+						$query->where('%n '.($not ? 'NOT ' : '').'BETWEEN %i AND %i', Strings::toSnakeCase($field), $values[0], $values[1]);
+					}
+					else if ($type === 'date') {
+						$query->where('%n '.($not ? 'NOT ' : '').'BETWEEN %dt AND %dt', Strings::toSnakeCase($field), new DateTime($values[0]), new DateTime($values[1]));
+					}
+					continue;
+				}
+
+				if (is_numeric($value)) { // Number
+					$query->where('%n '.$cmp.' %i', Strings::toSnakeCase($field), $value);
+				}
+				else if (strtotime($value) > 0) { // Date (time)
+					$query->where('%n '.$cmp.' %dt', Strings::toSnakeCase($field), new DateTime($value));
+				}
+				else { // String
+					if ($cmp !== '=' && $cmp !== '<>') {
+						$this->respond(
+							[
+								'error'       => 'Invalid filter',
+								'description' => 'Invalid comparator "'.$cmp.'" for string in field "'.$field.'".',
+								'value'       => $request->get['field'],
+							],
+							400);
+					}
+					$query->where('%n '.$cmp.' %s', Strings::toSnakeCase($field), $value);
+				}
+			}
+
+			// Return a raw SQL
+			// TODO: Limit this to admin access
+			if (isset($request->get['sql'])) {
+				$this->respond((string) $query);
+			}
+
+			$games = $query->fetchAll();
+		} catch (\InvalidArgumentException $e) {
+			$this->respond(['error' => 'Invalid input', 'exception' => $e->getMessage()], 400);
+		} catch (\Throwable $e) {
+			$this->respond(['error' => 'Unexpected error', 'exception' => $e->getMessage(), 'code' => $e->getCode()], 500);
+		}
+
+		// Return only public links
+		if (isset($request->get['returnLink'])) {
+			$links = [];
+			$prefix = trailingSlashIt(App::getLink(['g']));
+			foreach ($games as $game) {
+				$links[] = $prefix.$game->code;
+			}
+			$this->respond($links);
+		}
+
+		// Return only game codes
+		if (isset($request->get['returnCodes'])) {
+			$codes = [];
+			foreach ($games as $game) {
+				$codes[] = $game->code;
+			}
+			$this->respond($codes);
+		}
+
 		$this->respond($games);
 	}
 
