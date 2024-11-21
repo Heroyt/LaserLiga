a2599fe569eb2b8dce2deb0a848312c8b79a9d9d
Tomáš Vojík <vojik@wboy.cz>
2024-11-21 15:21:21 +0100

    :bug: Fixed CLI base URL

--- bin/scheduler.php
+++ bin/scheduler.php
@@ -1,6 +1,8 @@
 <?php
 
 use Lsr\Core\App;
+use Lsr\Core\Requests\Request;
+use Nyholm\Psr7\ServerRequest;
 use Orisai\Scheduler\Scheduler;
 
 const ROOT = __DIR__ . '/../';
@@ -8,5 +10,7 @@ const INDEX = false;
 
 require_once ROOT . "include/load.php";
 
+// For link generation and base URL to work
+App::getInstance()->setRequest(new Request(new ServerRequest('GET', 'https://laserliga.cz/')));
 $scheduler = App::getServiceByType(Scheduler::class);
 $scheduler->run();
