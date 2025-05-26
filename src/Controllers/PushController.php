2e79062abdc350b1cc318494044d12c6f2275536
Tomáš Vojík <vojik@wboy.cz>
2025-05-26 11:51:12 +0200

    :technologist: Enhance sendTest method to support multiple notification types

--- src/Controllers/PushController.php
+++ src/Controllers/PushController.php
@@ -2,12 +2,23 @@
 
 namespace App\Controllers;
 
+use App\GameModels\Factory\GameFactory;
+use App\GameModels\Game\Game;
+use App\GameModels\Game\Player;
+use App\Models\Achievements\Achievement;
+use App\Models\Achievements\PlayerAchievement;
+use App\Models\Auth\LigaPlayer;
 use App\Models\Auth\User;
+use App\Models\DataObjects\Game\PlayerGamesGame;
 use App\Models\Push\Notification;
 use App\Models\Push\Subscription;
 use App\Services\PushService;
+use DateTimeImmutable;
+use Lsr\Core\App;
 use Lsr\Core\Auth\Services\Auth;
 use Lsr\Core\Controllers\Controller;
+use Lsr\Core\Requests\Dto\ErrorResponse;
+use Lsr\Core\Requests\Enums\ErrorType;
 use Lsr\Core\Requests\Request;
 use Lsr\Orm\Exceptions\ValidationException;
 use Nette\Utils\Validators;
@@ -17,7 +28,7 @@ class PushController extends Controller
 {
 
 	/**
-	 * @param Auth<User>        $auth
+	 * @param Auth<User> $auth
 	 */
 	public function __construct(
 		private readonly Auth        $auth,
@@ -115,22 +126,78 @@ class PushController extends Controller
 		return $this->respond(['status' => 'ok']);
 	}
 
-	public function sendTest(): ResponseInterface {
+	public function sendTest(Request $request): ResponseInterface {
 		$user = $this->auth->getLoggedIn();
-		if (!isset($user)) {
+		if (!isset($user) || $user->player === null) {
 			return $this->respond(['error' => 'Not logged in'], 401);
 		}
 
-		$notification = new Notification();
-		$notification->user = $user;
-		$notification->title = 'Test Notifikace';
-		$notification->body = 'Tohle je testovací notifikace';
+		$type = trim(strtolower((string) $request->getGet('type', 'test')));
 
-		$this->pushService->send($notification);
+		switch ($type) {
+			case 'rank_change':
+				$this->pushService->sendRankChangeNotification(
+					$user->player,
+					random_int(-5, 5),
+					random_int(1, 50) . '.',
+				);
+				break;
+			case 'achievement':
+				$game = $this->getRandomGame($user->player);
+				if ($game === null) {
+					return $this->respond(new ErrorResponse('Game not found', ErrorType::NOT_FOUND), 404);
+				}
+				$achievements = [];
+				$achievementModels = Achievement::query()->orderBy('RAND()')->limit(random_int(1, 5))->get(false);
+				foreach ($achievementModels as $achievementModel) {
+					$achievements[] = new PlayerAchievement(
+						$achievementModel,
+						$user->player,
+						$game,
+						new DateTimeImmutable()
+					);
+				}
+				$this->pushService->sendAchievementNotification(...$achievements);
+				break;
+			case 'photos':
+				$game = $this->getRandomGame($user->player);
+				if ($game === null) {
+					return $this->respond(new ErrorResponse('Game not found', ErrorType::NOT_FOUND), 404);
+				}
+				$this->pushService->sendPhotosNotification($user->player, $game, App::getLink(['games', $game->code]));
+				break;
+			case 'game':
+				$game = $this->getRandomGame($user->player);
+				if ($game === null) {
+					return $this->respond(new ErrorResponse('Game not found', ErrorType::NOT_FOUND), 404);
+				}
+				$player = $game->players->first(static fn(Player $player) => $player->user->id === $user->id);
+				assert($player !== null);
+				$this->pushService->sendNewGameNotification($player, $user->player);
+				break;
+			default:
+				$notification = new Notification();
+				$notification->user = $user;
+				$notification->title = 'Test Notifikace';
+				$notification->body = 'Tohle je testovací notifikace';
 
-		$notification->save();
+				$this->pushService->send($notification);
+
+				$notification->save();
+		}
 
 		return $this->respond(['status' => 'ok']);
+	}
+
+	private function getRandomGame(LigaPlayer $player): ?Game {
+		/** @var PlayerGamesGame|null $gameRow */
+		$gameRow = $player->queryGames()
+		                  ->orderBy('RAND()')
+		                  ->fetchDto(PlayerGamesGame::class, false);
+		if ($gameRow === null) {
+			return null;
+		}
+		return GameFactory::getByCode($gameRow->code);
 	}
 
 }
