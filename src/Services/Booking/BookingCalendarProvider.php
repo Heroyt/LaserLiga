9d64f5210f08bfccaf8fe10815d1ed3b4d67999f
Tomáš Vojík <vojik@wboy.cz>
2025-07-30 13:45:01 +0200

    :sparkles: Add BookingCalendarProvider with date availability check

--- src/Services/Booking/BookingCalendarProvider.php
+++ src/Services/Booking/BookingCalendarProvider.php
@@ -3,7 +3,41 @@ declare(strict_types=1);
 
 namespace App\Services\Booking;
 
+use App\CQRS\Queries\Booking\OpenHoursForDateQuery;
+use App\Models\Booking\BookingType;
+use DateTimeInterface;
+use Lsr\Core\App;
+
 class BookingCalendarProvider
 {
+
+	/** @var array<string, bool>[] */
+	private array $isDateOpen = [];
+
+
+	/**
+	 * Check if a specific date is open for bookings of a given type.
+	 *
+	 * @param DateTimeInterface $date
+	 * @param BookingType       $type
+	 *
+	 * @return bool
+	 */
+	public function isDateOpen(DateTimeInterface $date, BookingType $type, bool $cache = true): bool {
+		$formatted = $date->format('Y-m-d');
+		if (!$cache || !isset($this->isDateOpen[$type->id][$formatted])) {
+			$query = new OpenHoursForDateQuery($type->arena, $date);
+			$query->type($type)
+			      ->includeNormalHours()
+			      ->includeSpecialHours();
+
+			if (!$cache) {
+				$query->noCache();
+			}
+
+			$this->isDateOpen[$type->id][$formatted] = !empty($query->get());
+		}
+		return $this->isDateOpen[$type->id][$formatted];
+	}
 
 }
