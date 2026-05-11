cfb69de88899f82822ad856569db7b0d29d29821
Tomáš Vojík <vojik@wboy.cz>
2026-05-11 16:19:54 +0200

    :sparkles: Add Tracy Debugger integration and error handling in scheduler and console
    - Integrated Tracy Debugger for development mode in the console.
    - Added error handling to the scheduler with logging as warnings.

--- bin/scheduler.php
+++ bin/scheduler.php
@@ -4,6 +4,9 @@ use Lsr\Core\App;
 use Lsr\Core\Requests\Request;
 use Nyholm\Psr7\ServerRequest;
 use Orisai\Scheduler\Scheduler;
+use Tracy\Debugger;
+use Tracy\Helpers;
+use Tracy\ILogger;
 
 const ROOT = __DIR__ . '/../';
 const INDEX = false;
@@ -13,4 +16,10 @@ require_once ROOT . "include/load.php";
 // For link generation and base URL to work
 App::getInstance()->setRequest(new Request(new ServerRequest('GET', 'https://laserliga.cz/')));
 $scheduler = App::getServiceByType(Scheduler::class);
-$scheduler->run();
+
+try {
+	$scheduler->run();
+} catch (Throwable $e) {
+	Helpers::improveException($e);
+	Debugger::log($e, ILogger::WARNING); // Log, but as a warning to not send an email.
+}
